57972fc61517cf30d7ae33bf89a9970c44a000ed
kavincu27 <kavincu27@gmail.com>
2026-04-06 09:04:49 +0530

    Initial commit

---
+++ src/main/java/org/example/level1/q3.java
@@ -0,0 +1,10 @@
+package org.example.level1;
+
+public class q3 {
+    public static void main(String[] args) {
+        int pen=14;
+        int PenPerStudent=pen/3;
+        int BalancePen=pen%3;
+        System.out.println("The Pen Per Student is "+PenPerStudent+" and the remaining pen not distributed is "+BalancePen);
+    }
+}
