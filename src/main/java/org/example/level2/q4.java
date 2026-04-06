554f974b97904703ee05b8b69cdba3610fd809d3
kavincu27 <kavincu27@gmail.com>
2026-04-06 09:35:24 +0530

    [kavin]added: java code for level2

---
+++ src/main/java/org/example/level2/q4.java
@@ -0,0 +1,18 @@
+package org.example.level2;
+
+public class q4 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a : ");
+        int a=sc.nextInt();
+        System.out.println("Enter b : ");
+        int b=sc.nextInt();
+        System.out.println("Enter c : ");
+        int c=sc.nextInt();
+        int r1= a + b *c;
+        int r2=a * b + c;
+        int r3=c + a / b;
+        int r4=a % b + c;
+        System.out.println("The results of Int Operations are "+r1+" , "+r2+" , "+r3+" and "+r4);
+    }
+}
---
+++ src/main/java/org/example/level2/q5.java
@@ -0,0 +1,18 @@
+package org.example.level2;
+
+public class q5 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a : ");
+        double a=sc.nextDouble();
+        System.out.println("Enter b : ");
+        double b=sc.nextDouble();
+        System.out.println("Enter c : ");
+        int c=sc.nextInt();
+        double r1= a + b *c;
+        double r2=a * b + c;
+        double r3=c + a / b;
+        double r4=a % b + c;
+        System.out.println("The results of Int Operations are "+r1+" , "+r2+" , "+r3+" and "+r4);
+    }
+}
